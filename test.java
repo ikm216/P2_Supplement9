0d602a59e25339507538e69f6f936230c0e04952
ikm216 <ikmclassof22@gmail.com>
2024-12-08 17:33:47 -0500

    test: added test to get even numbers from a list.

--- test.java
+++ test.java
@@ -1,5 +1,12 @@
 import org.junit.*;
+import java.util.*;
 
 public class test{
-    
+    @Test
+    public void test_should_return_even_numbers(){
+        List<Integer> nums = Arrays.asList(1, 2, 4, 7, 8);
+        nums = lang.evenNumbers(nums);
+        List<Integer> test = Arrays.asList(2, 4, 8);
+        Assert.assertEquals(test, nums);
+    }
 }
