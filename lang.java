091d9a6c66786ff7a7bfad9ec5b8d9949ca70383
ikm216 <ikmclassof22@gmail.com>
2024-12-08 17:40:33 -0500

    feat: added feature to get even numbers from a list.

--- lang.java
+++ lang.java
@@ -1,3 +1,13 @@
+import java.util.*;
+
 public class lang {
-    
+    public static List<Integer> evenNumbers(List<Integer> nums){
+        List<Integer> even_nums = new ArrayList<>();
+        for(int num: nums){
+            if(num % 2 == 0){
+                even_nums.add(num);
+            }
+        }
+        return even_nums;
+    }
 }
